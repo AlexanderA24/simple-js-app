5eac7f5afa1fe7c89d159f2b0dc3e910d5b0128e
alexander-bespokelogic <71767279+alexander-bespokelogic@users.noreply.github.com>
2022-02-12 13:39:23 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World!');
-
-let favortieFood  = "Tofu"
-document.write(favortieFood)
+let pokemonList = [
+    {name: "Pikachu", type:["electric","mouse"], height:2}, 
+    {name: "Squirtle", type:["water","turtle"], height:3},
+    {name: "Bulbasoar", type:["Grass", "monster"], height:1}
+]
