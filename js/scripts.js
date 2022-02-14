4f84559f8264e8804fc0d318feb35513f56c3bcd
alexander-bespokelogic <71767279+alexander-bespokelogic@users.noreply.github.com>
2022-02-14 18:47:16 +0000

    add favourite food variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-let pokemonList = [
-    {name: "Pikachu", type:["electric","mouse"], height:2}, 
-    {name: "Squirtle", type:["water","turtle"], height:3},
-    {name: "Bulbasoar", type:["Grass", "monster"], height:1}
-]
+alert('Hello World!');
+
+favouriteFood = 'Indian food'
+
+document.write(favouriteFood)
+
