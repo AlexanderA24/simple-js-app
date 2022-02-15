05bb469582c83a3da0e9dc14743c63653cd45580
alexander-bespokelogic <71767279+alexander-bespokelogic@users.noreply.github.com>
2022-02-15 13:48:32 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
+// Create pokemon list object //
 let pokemonList = [
     {name: "Pikachu", type:["electric","mouse"], height:2}, 
     {name: "Squirtle", type:["water","turtle"], height:3},
     {name: "Bulbasoar", type:["Grass", "monster"], height:1}
 ]
+
+// Loop through each pokemon and output name and height on new lines //
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i]["height"] > 2  ) {
+        document.write(pokemonList[i]["name"].concat(" height:", 
+            pokemonList[i]["height"]).concat("- Wow, that's big! <br>"))
+    }  else {
+        document.write(pokemonList[i]["name"].concat(" height:", 
+            pokemonList[i]["height"]).concat("<br>"))
+    }
+}
+
