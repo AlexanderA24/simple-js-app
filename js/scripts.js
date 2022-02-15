14d87643f7f0f399169fe93564f84b9f6ca943ee
alexander-bespokelogic <71767279+alexander-bespokelogic@users.noreply.github.com>
2022-02-15 15:32:05 +0000

    part 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,31 @@ let pokemonList = [
     {name: "Pikachu", type:["electric","mouse"], height:2}, 
     {name: "Squirtle", type:["water","turtle"], height:3},
     {name: "Bulbasoar", type:["Grass", "monster"], height:1}
+<<<<<<< Updated upstream
 ]
+=======
+]
+
+// // Loop through each pokemon and output name and height on new lines //
+// for (let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i]["height"] > 2  ) {
+//         document.write(pokemonList[i]["name"].concat(" height:", 
+//             pokemonList[i]["height"]).concat("- Wow, that's big! <br>"))
+//     }  else {
+//         document.write(pokemonList[i]["name"].concat(" height:", 
+//             pokemonList[i]["height"]).concat("<br>"))
+//     }
+// }
+
+function listPokemon(pokemon) {
+    if (pokemon["height"] > 2  ) {
+        document.write(pokemon["name"].concat(" height:", 
+            pokemon["height"]).concat("- Wow, that's big! <br>"))
+    }  else {
+        document.write(pokemon["name"].concat(" height:", 
+            pokemon["height"]).concat("<br>"))
+    }
+}
+
+pokemonList.forEach(listPokemon);
+>>>>>>> Stashed changes
