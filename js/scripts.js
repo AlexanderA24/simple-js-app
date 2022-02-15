c370c4737c8ec33c01eafa5e8f81ad55f6723496
alexander-bespokelogic <71767279+alexander-bespokelogic@users.noreply.github.com>
2022-02-15 18:04:45 +0000

    Bonus section
    Added function to check if user inputs object type
    Added function to validate that correct keys are passed
    Added function to search by pokemon name

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,86 @@
-let pokemonRepository = ( function () {
+let pokemonRepository = (function () {
     let pokemonList = [
-        {name: "Pikachu", type:["electric","mouse"], height:2}, 
-        {name: "Squirtle", type:["water","turtle"], height:3},
-        {name: "Bulbasoar", type:["Grass", "monster"], height:1}
-    ]
-
+      { name: "Pikachu", type: ["electric", "mouse"], height: 2 },
+      { name: "Squirtle", type: ["water", "turtle"], height: 3 },
+      { name: "Bulbasoar", type: ["Grass", "monster"], height: 1 },
+    ];
+  
+    function arrayEquals(a, b) {
+      return (
+        Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index])
+      );
+    }
+  
     function add(pokemon) {
-        pokemonList.push(pokemon);
+      // Check if variable pushed is of object type //
+      if (
+        typeof pokemon === "object" &&
+        !Array.isArray(pokemon) &&
+        pokemon !== null
+      ) {
+        // Check if Pushed keys match pokemonList keys //
+        let pokeKeys = ["name", "type", "height"];
+        let pushedobject = Object.keys(pokemon);
+  
+        if (arrayEquals(pushedobject, pokeKeys)) {
+          // Check if data types match //
+          if (
+            typeof pokemon["name"] === "string" &&
+            Array.isArray(pokemon["type"]) &&
+            typeof pokemon["height"] === "number"
+          ) {
+            pokemonList.push(pokemon);
+            console.log("Success");
+          } else {
+            console.log(
+              `Please make sure date types are as follows "Name":String, "type":array, "height":int`
+            );
+          }
+        } else {
+          console.log(
+            `Please use appropriate keys being "Name(string)", "Type(array)", "Height(int)"`
+          );
+        }
+      } else {
+        console.log("Please add an object value");
+      }
     }
-
+  
     function getAll() {
-        return pokemonList;
-    
+      return pokemonList;
     }
+  
+    function search(query) {
+      var filteredList = pokemonList.filter(function (pokemon) {
+        return pokemon.name == query;
+      });
+  
+      return console.log(filteredList);
+    }
+  
     return {
-        add:add,
-        getAll: getAll
+      add: add,
+      getAll: getAll,
+      search: search,
     };
-})();
-
-// pokemonRepository.add({name: "Charizard",type:["fire","dragon"],height:12});
-
-function listPokemon(pokemon) {
-    if (pokemon["height"] > 2  ) {
-        document.write(pokemon["name"].concat(" height:", 
-            pokemon["height"]).concat("- Wow, that's big! <br>"))
-    }  else {
-        document.write(pokemon["name"].concat(" height:", 
-            pokemon["height"]).concat("<br>"))
+  })();
+  
+  function listPokemon(pokemon) {
+    if (pokemon["height"] > 2) {
+      document.write(
+        pokemon["name"]
+          .concat(" height:", pokemon["height"])
+          .concat("- Wow, that's big! <br>")
+      );
+    } else {
+      document.write(
+        pokemon["name"].concat(" height:", pokemon["height"]).concat("<br>")
+      );
     }
-}
-
-pokemonRepository.getAll().forEach(listPokemon);
-
+  }
+  
+  pokemonRepository.getAll().forEach(listPokemon);
+  
